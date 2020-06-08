3025cd230ef3adfbbaa6b42e59f2f60d3620efaf
neetsh24 <tamrakarnitesh21@gmail.com>
2020-06-08 14:10:09 +0545

    Express REST API with Mongoose Part 1

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter=require('./routes/dishRouter');
+var promoRouter=require('./routes/promoRouter');
+var leaderRouter=require('./routes/leaderRouter');
+
+//not connected to backend mongodb server, so to establish connection
+const mongoose=require('mongoose');
+
+//import dishes
+const Dishes=require('./modals/dishes');
+//establish connection with server
+const url='mongodb://localhost:27017/conFusion';
+const connect=mongoose.connect(url);
+
+connect.then((db)=>{
+  console.log('Connection to the server succesfull!!!');
+},(err)=>{console.log(err);});
+
 
 var app = express();
 
@@ -21,6 +38,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
